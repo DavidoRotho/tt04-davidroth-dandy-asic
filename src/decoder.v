7882f64ee5f82495f8f1535d89a6e0eeda2e1fe3
Blaise <blaise.s.saunders@gmail.com>
2023-09-05 09:30:49 +0000

    wip: serial into graphics instructions

--- src/decoder.v
+++ src/decoder.v
@@ -1,3 +1,4 @@
+
 // Instructions
 parameter C_NOP      = 3'd0;
 parameter C_LINE     = 3'd1;
@@ -15,6 +16,7 @@ parameter MAX_INSTRUCTIONS = 8'd7;
 module triangle_wave_gen_fsm (
     input clk,
     input reset, // Active high reset
+    input enable,
     input [(INSTRUCTION_COUNT*3)-1:0] instructions_flat,
     input [(INSTRUCTION_COUNT*8)-1:0] params_flat_a,
     input [(INSTRUCTION_COUNT*8)-1:0] params_flat_b,
@@ -64,7 +66,7 @@ module triangle_wave_gen_fsm (
     endgenerate
 
     always @(posedge clk) begin
-        if (reset) begin
+        if (reset || !enable) begin
             action_state <= UP;
             logic_state = L_INIT;
             cycle_flag = 1'b0;
@@ -77,38 +79,37 @@ module triangle_wave_gen_fsm (
                 DOWN: counter = counter - 8'd1;
                 HOLD: counter = counter;
             endcase
-            param_counter = param_counter + 8'd1;
+            param_counter = param_counter + 8'd1; // Used by instructions to check when to finish
             cycle_flag = 1; // Switch to check cycle
         end else begin // Check cycle
             
-            // Werk
+            // Here we will pick an instruction to execture
             case (instructions[instruction_pointer])
                 C_NOP: begin
                     action_state <= HOLD;
                     done_op = 1'b1;
                 end
                 C_LINE: begin
-                    action_state <= HOLD;
-                    counter = 8'd240;
-                    done_op = 1'b1;
-//                    case (logic_state)
-//                        L_INIT: begin
-////                            if (params_a[instruction_pointer] > params_b[instruction_pointer]) begin
-//                                action_state <= UP;
-////                            end else begin
-////                                action_state <= DOWN;
-////                            end
-//                            counter = params_a[instruction_pointer];
-//                            logic_state = L_ONE;
-//                        end
-//                        L_ONE: begin
-//                            if (param_counter == params_b[instruction_pointer]) begin
-//                                logic_state = L_NOP;
-//                                param_counter = 4'd0;
-//                                done_op = 1'b1;
-//                            end
-//                        end
-//                    endcase
+                    case (logic_state)
+                        L_INIT: begin
+                            if (params_a[instruction_pointer] > params_b[instruction_pointer]) begin
+                                action_state <= UP;
+                            end else begin
+                                action_state <= DOWN;
+                            end
+                            counter = params_a[instruction_pointer];
+                            logic_state = L_ONE;
+                            param_counter = 8'd0;
+                        end
+                        L_ONE: begin
+                            if (param_counter == params_b[instruction_pointer])
+                            begin
+                                //logic_state = L_NOP;
+                                action_state <= HOLD;
+                                done_op = 1'b1;
+                            end
+                        end
+                    endcase
                 end
                 C_JUMP: begin
                     action_state <= HOLD;
@@ -212,60 +213,238 @@ module triangle_wave_gen_fsm (
 
 endmodule
 
-
-
-module image_wave_gen (
+module triangle_wave_gen_fsm_comb (
     input clk,
-    input reset, // Active high reset
-    output [7:0] xdac,
-    output [7:0] ydac
+    input reset, 
+    input enable,
+    input [(INSTRUCTION_COUNT*3)-1:0] instructions_flat,
+    input [(INSTRUCTION_COUNT*8)-1:0] params_flat_a,
+    input [(INSTRUCTION_COUNT*8)-1:0] params_flat_b,
+    output reg [7:0] dac_out
 );
 
-    reg [(INSTRUCTION_COUNT*3)-1:0] x_instructions_flat;
-    reg [(INSTRUCTION_COUNT*8)-1:0] x_params_a_flat;
-    reg [(INSTRUCTION_COUNT*8)-1:0] x_params_b_flat;
-        
-    reg [(INSTRUCTION_COUNT*3)-1:0] y_instructions_flat;
-    reg [(INSTRUCTION_COUNT*8)-1:0] y_params_a_flat;
-    reg [(INSTRUCTION_COUNT*8)-1:0] y_params_b_flat;
+    reg [7:0] counter;
+    reg [7:0] next_counter;
+    reg [4:0] logic_state, next_logic_state;
+    reg [2:0] action_state, next_action_state;
+    reg cycle_flag, next_cycle_flag; 
+    reg done_op, next_done_op;
     
+    wire [2:0] instructions[INSTRUCTION_COUNT-1:0];
+    wire [7:0] params_a[INSTRUCTION_COUNT-1:0];
+    wire [7:0] params_b[INSTRUCTION_COUNT-1:0];
+    reg [7:0] param_counter, next_param_counter;
+    reg [INSTRUCTION_COUNT-1:0] instruction_pointer, next_instruction_pointer;
     
+    // Constants for clarity
+    parameter MAX_COUNTER_VALUE = 8'd255;  // Maximum value for 8-bit up count in decimal
+    parameter MIN_COUNTER_VALUE = 8'd0;    // Minimum value for 8-bit down count in decimal
+    parameter BOUNCE_VALUE      = 8'd128;
+
+    // Action states
+    parameter UP   = 2'b00;
+    parameter DOWN = 2'b01;
+    parameter HOLD = 2'b10;
+    parameter JUMP = 2'b11;
     
+    // Logic states
+    parameter L_INIT   = 4'd0;
+    parameter L_ONE    = 4'd1;
+    parameter L_TWO    = 4'd2;
+    parameter L_THREE  = 4'd3;
+    parameter L_FOUR   = 4'd4;
+    parameter L_NOP    = 4'd15;
+
+    genvar i;
+    generate
+        for (i = 0; i < INSTRUCTION_COUNT; i = i + 1) begin
+            assign instructions[i] = instructions_flat[i*3 +: 3];
+            assign params_a[i] = params_flat_a[i*PARAM_SIZE +: PARAM_SIZE];
+            assign params_b[i] = params_flat_b[i*PARAM_SIZE +: PARAM_SIZE];
+        end
+    endgenerate
+
+    // Combinatorial Logic
+    always @(posedge clk) begin
+
+        // The previous FSM logic goes here, but with "next_" prefixes for all state variables.
+        // I'm condensing this for brevity, but essentially you'll adapt the original FSM to this section.
+        // You would replace direct assignments to action_state, logic_state, etc., with assignments
+        // to next_action_state, next_logic_state, etc.
+
+        // For example, this:
+        // if (reset) action_state = UP;
+        // would become:
+        // if (reset) next_action_state = UP;
+
+        // ... [rest of the FSM logic from your original code, adapted with the next_ prefix for state variables]
+         if (cycle_flag == 0) begin // State cycle
+             if (reset) begin
+                action_state <= UP;
+                logic_state <= L_INIT;
+                cycle_flag <= 1'b0;
+                done_op <= 1'b0;
+                instruction_pointer <= 8'd0;
+                param_counter <= 8'd0;
+                counter <= 8'd0;
+                dac_out <= 8'd0;
+            end else if (enable) begin
+                action_state <= next_action_state;
+                logic_state <= next_logic_state;
+                //cycle_flag <= next_cycle_flag;
+                done_op <= next_done_op;
+                instruction_pointer <= next_instruction_pointer;
+                param_counter <= next_param_counter;
+                counter <= next_counter;
+                dac_out <= counter;
+            end
+
+            cycle_flag = 1; // Switch to check cycle
+        end else begin // Check cycle
+            next_action_state = action_state;
+            next_logic_state = logic_state;
+            next_cycle_flag = cycle_flag;
+            next_done_op = done_op;
+            next_instruction_pointer = instruction_pointer;
+            next_param_counter = param_counter;
+            next_counter = counter;
+
+            case (action_state)
+              UP: next_counter = counter + 8'd1;
+                DOWN: next_counter = counter - 8'd1;
+                HOLD: next_counter = counter;
+            endcase
+            next_param_counter = param_counter + 8'd1; // Used by instructions to check when to finish
+            // Here we will pick an instruction to execture
+            case (instructions[instruction_pointer])
+                C_NOP: begin
+                    next_action_state = HOLD;
+                    next_done_op = 1'b1;
+                end
+                C_LINE: begin
+                    case (logic_state)
+                        L_INIT: begin
+                            if (params_a[instruction_pointer] > params_b[instruction_pointer]) begin
+                                next_action_state = UP;
+                            end else begin
+                                next_action_state = DOWN;
+                            end
+                            next_counter = params_a[instruction_pointer];
+                            next_logic_state = L_ONE;
+                            next_param_counter = 8'd0;
+                        end
+                        L_ONE: begin
+                            if (param_counter == params_b[instruction_pointer])
+                            begin
+                                //logic_state = L_NOP;
+                                next_action_state = HOLD;
+                                next_done_op = 1'b1;
+                            end
+                        end
+                    endcase
+                end
+                C_JUMP: begin
+                    next_action_state = HOLD;
+                    next_counter = params_a[instruction_pointer];
+                    next_done_op = 1'b1;
+                end
+                C_INCR: begin
+                    next_action_state = UP;
+                    if (param_counter == params_a[instruction_pointer])
+                        next_done_op = 1'b1;
+                end
+                C_DCRE: begin
+                    next_action_state = DOWN;
+                    if (param_counter == params_a[instruction_pointer])
+                        next_done_op = 1'b1;
+                end
+                C_X_RECT: begin
+                    case (logic_state)
+                        L_INIT: begin
+                            next_action_state = UP;
+                            if (param_counter == params_a[instruction_pointer]) begin
+                                next_logic_state = L_ONE;
+                                next_param_counter = 4'd0;
+                            end
+                        end
+                        L_ONE: begin
+                            next_action_state = HOLD;
+                            if (param_counter == params_b[instruction_pointer]) begin
+                                next_logic_state = L_TWO;
+                                next_param_counter = 4'd0;
+                            end
+                        end
+                        L_TWO: begin
+                            next_action_state = DOWN;
+                            if (param_counter == params_a[instruction_pointer]) begin
+                                next_logic_state = L_THREE;
+                                next_param_counter = 4'd0;
+                            end
+                        end
+                        L_THREE: begin
+                            next_action_state = HOLD;
+                            if (param_counter == params_b[instruction_pointer]) begin
+                                next_logic_state = L_NOP;
+                                next_param_counter = 4'd0;
+                                next_done_op = 1'b1;
+                            end
+                        end
+                    endcase
+                end
+                C_Y_RECT: begin
+                    case (logic_state)
+                        L_INIT: begin
+                            next_action_state = HOLD;
+                            if (param_counter == params_a[instruction_pointer]) begin
+                                next_logic_state = L_ONE;
+                                next_param_counter = 4'd0;
+                            end
+                        end
+                        L_ONE: begin
+                            next_action_state = UP;
+                            if (param_counter == params_b[instruction_pointer]) begin
+                                next_logic_state = L_TWO;
+                                next_param_counter = 4'd0;
+                            end
+                        end
+                        L_TWO: begin
+                            next_action_state = HOLD;
+                            if (param_counter == params_a[instruction_pointer]) begin
+                                next_logic_state = L_THREE;
+                                next_param_counter = 4'd0;
+                            end
+                        end
+                        L_THREE: begin
+                            next_action_state = DOWN;
+                            if (param_counter == params_b[instruction_pointer]) begin
+                                next_logic_state = L_NOP;
+                                next_param_counter = 4'd0;
+                                next_done_op = 1'b1;
+                            end
+                        end
+                    endcase
+                end
+            endcase       
     
-    // DONT ASSUME IT'LL START FROM 0 LIKE ON THE FPGA
-    always @(*) begin
-        x_instructions_flat   = {C_X_RECT, C_JUMP, C_X_RECT, C_JUMP, C_X_RECT, C_JUMP, C_X_RECT, C_JUMP};
-        x_params_a_flat       = {8'd100,   8'd150, 8'd75,    8'd100, 8'd030,   8'd030, 8'd40, 8'd000};
-        x_params_b_flat       = {8'd100,   8'd150, 8'd75,    8'd000, 8'd030,   8'd030, 8'd40, 8'd000};
-        
-        y_instructions_flat   = {C_Y_RECT, C_JUMP, C_Y_RECT, C_JUMP, C_Y_RECT, C_JUMP, C_Y_RECT, C_JUMP};
-        y_params_a_flat       = {8'd100,   8'd150, 8'd75,    8'd000, 8'd030,   8'd100, 8'd40, 8'd000};
-        y_params_b_flat       = {8'd100,   8'd150, 8'd75,    8'd000, 8'd030,   8'd100, 8'd40, 8'd000};
+            // Check if this instruction is done
+            if (done_op == 1'b1) begin
+//                if (instruction_pointer == MAX_INSTRUCTIONS) instruction_pointer = 3'd0;
+//                else instruction_pointer = instruction_pointer + 8'd1;
+
+                next_instruction_pointer = instruction_pointer + 8'd1; // Loops
+                next_param_counter = 8'd0;
+                next_logic_state = L_INIT;
+
+                next_done_op = 1'b0;
+            end
+            cycle_flag = 0; // Switch back to action cycle
+        end
     end
-
-    
-    triangle_wave_gen_fsm triangle1 (
-        .clk(clk),
-        .reset(reset),
-        .instructions_flat(x_instructions_flat),
-        .params_flat_a(x_params_a_flat),
-        .params_flat_b(x_params_b_flat),
-        .dac_out(xdac)
-    );
-    
-    triangle_wave_gen_fsm triangle2 (
-        .clk(clk),
-        .reset(reset),
-        .instructions_flat(y_instructions_flat),
-        .params_flat_a(y_params_a_flat),
-        .params_flat_b(y_params_b_flat),
-        .dac_out(ydac)
-    );
-
-
 endmodule
 
 
+
+// RX code from fpga4fun.com, all credit to them!! :) :) THANKS GUYS
 ////////////////////////////////////////////////////////
 module async_receiver(
 	input clk,
@@ -394,3 +573,58 @@ always @(posedge clk) if(enable) Acc <= Acc[AccWidth-1:0] + Inc[AccWidth:0]; els
 assign tick = Acc[AccWidth];
 endmodule
 
+
+module image_wave_gen (
+    input clk,
+    input reset, // Active high reset
+    input [7:0]uart_rx,
+    output [7:0] xdac,
+    output [7:0] ydac
+);
+
+    reg [(INSTRUCTION_COUNT*3)-1:0] x_instructions_flat;
+    reg [(INSTRUCTION_COUNT*8)-1:0] x_params_a_flat;
+    reg [(INSTRUCTION_COUNT*8)-1:0] x_params_b_flat;
+        
+    reg [(INSTRUCTION_COUNT*3)-1:0] y_instructions_flat;
+    reg [(INSTRUCTION_COUNT*8)-1:0] y_params_a_flat;
+    reg [(INSTRUCTION_COUNT*8)-1:0] y_params_b_flat;
+    reg enable = 0;
+    
+    
+    
+    // DONT ASSUME IT'LL START FROM 0 LIKE ON THE FPGA
+    always @(posedge clk) begin
+        x_instructions_flat   = {C_X_RECT, C_JUMP, C_X_RECT, C_JUMP, C_X_RECT, C_JUMP, C_X_RECT, C_JUMP};
+        x_params_a_flat       = {8'd100,   8'd150, 8'd75,    8'd100, uart_rx,   8'd030, 8'd90, 8'd000};
+        x_params_b_flat       = {8'd100,   8'd150, 8'd75,    8'd100, uart_rx,   8'd030, 8'd90, 8'd000};
+        
+        y_instructions_flat   = {C_Y_RECT, C_JUMP, C_Y_RECT, C_JUMP, C_Y_RECT, C_JUMP, C_Y_RECT, C_JUMP};
+        y_params_a_flat       = {8'd100,   8'd150, 8'd75,    8'd020, uart_rx,   8'd000, 8'd90, 8'd000};
+        y_params_b_flat       = {8'd100,   8'd150, 8'd75,    8'd020, uart_rx,   8'd000, 8'd90, 8'd000};
+        enable = 1;
+    end
+
+    
+    triangle_wave_gen_fsm_comb triangle1 (
+        .clk(clk),
+        .reset(reset),
+        .enable(enable),
+        .instructions_flat(x_instructions_flat),
+        .params_flat_a(x_params_a_flat),
+        .params_flat_b(x_params_b_flat),
+        .dac_out(xdac)
+    );
+    
+    triangle_wave_gen_fsm_comb triangle2 (
+        .clk(clk),
+        .reset(reset),
+        .enable(enable),
+        .instructions_flat(y_instructions_flat),
+        .params_flat_a(y_params_a_flat),
+        .params_flat_b(y_params_b_flat),
+        .dac_out(ydac)
+    );
+
+
+endmodule
--- src/tt_um_dandy_dance.v
+++ src/tt_um_dandy_dance.v
@@ -1,5 +1,5 @@
 
-module tt_um_dandy_dance (
+module tt_um_dandy_dance #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -15,14 +15,46 @@ module tt_um_dandy_dance (
     wire [7:0] y_out;
     assign uo_out[7:0] = x_out;
     assign uio_out[7:0] = y_out;
-    
     assign uio_oe = 8'b11111111;
+    
+    // Instantiate UART RX module
+    wire rx_data_ready;
+    wire idle;
+    wire eop;
+    wire uclk = clk;
+    wire [7:0]rx_data_r;
+    reg [7:0]rx_data_out;
+
+    always @(posedge rx_data_ready) rx_data_out <= rx_data_r;
+
+    async_receiver uart_rx_m(
+        .clk(uclk),
+        .RxD(ui_in[0]),
+        .RxD_data_ready(rx_data_ready),
+        .RxD_data(rx_data_r),
+        .RxD_idle(idle),
+        .RxD_endofpacket(eop)
+    );
+
+    reg [5:0] clk_divider = 0;
+    reg div_clk_unsynced;
+    reg div_clk_sync_stage1;
+    reg div_clk; // Using reg for div_clk
+
+    always @(posedge clk) begin
+        clk_divider <= clk_divider + 4'd1;
+        div_clk_unsynced <= (clk_divider == 5'd0);
+        div_clk_sync_stage1 <= div_clk_unsynced;
+        div_clk <= div_clk_sync_stage1;
+    end
 
     image_wave_gen im_gen(
-        .clk(clk),
-        .reset(reset), // Active high reset
+        .clk(div_clk),
+        .reset(reset),
+        .uart_rx(rx_data_out),
         .xdac(x_out),
         .ydac(y_out)
     );
-
+    
 endmodule
+
